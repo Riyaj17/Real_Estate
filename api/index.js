d3f81eea022421d7a5bc2e19ec4c968bb83e64b6
riyaj17 <riyajmondal2019@gmail.com>
2024-07-27 00:55:28 +0530

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express'; //after install the express then import it
 import mongoose from 'mongoose'; //after install mongoose then import it 
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js'; // export default router but here the name is userRouter but here use the same address , that's why no problem occurs
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() =>{
@@ -14,5 +15,7 @@ mongoose.connect(process.env.MONGO).then(() =>{
 const app = express();
 app.listen(3000, () => {
     console.log('Server is running on port 3000!');
-    }
-)
+});
+
+//create a api route 
+app.use("/api/user", userRouter); // userRouter has a default export address , and here use the name userRouter
