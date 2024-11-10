668f350c03d826a6e572fb9208e137fc0804e346
riyaj17 <riyajmondal2019@gmail.com>
2024-11-11 00:51:42 +0530

    Create the listing item component and show listings

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html", //this is basically check the index.html file ,,,,also all the file inside the src folder and any tailwind css class you add is going to be detected and css equivalent of that is going to be created
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [
+    require('@tailwindcss/line-clamp'),
+    // ...
+  ],
+};
